f2ce3c982f92c8662c1b168bab0a8c6f49a90dca
Amelia Eckard <120612539+ameliaeckard@users.noreply.github.com>
2026-09-25 14:03:57 -0400

    Remove duplicate Essentials /hat command (#2)
    * Stop registering hat from Essentials
    
    * Remove duplicate Essentials hat implementation
    
    * Remove Essentials hat command declaration

--- src/main/java/com/herasgarden/gardenessentials/GardenEssentials.java
+++ src/main/java/com/herasgarden/gardenessentials/GardenEssentials.java
@@ -57,7 +57,7 @@ public final class GardenEssentials extends JavaPlugin {
                 "balance", "pay", "spawn", "setspawn",
                 "warp", "warps", "setwarp", "delwarp",
                 "tpa", "tpaccept", "tpdeny",
-                "msg", "reply", "back", "afk", "heal", "feed", "fly", "hat")) {
+                "msg", "reply", "back", "afk", "heal", "feed", "fly")) {
             PluginCommand command = getCommand(commandName);
             if (command != null) {
                 command.setExecutor(utilities);
